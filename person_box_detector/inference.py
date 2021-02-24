- fix resizing formula - fix function parameters

person_box_detector/inference.py
@@ -11,7 +11,7 @@ import torch
 from torch.autograd import Variable
 from torchvision.transforms import Compose, Resize, Pad, ToTensor
 
-from .models import Darknet
+from person_box_detector.models import Darknet
 from configs.make_config import Config
 from person_box_detector.utils.utils import non_max_suppression, load_classes
 
@@ -20,7 +20,7 @@ from person_box_detector.utils.utils import non_max_suppression, load_classes
 Tensor = torch.FloatTensor
 
 
-def detect_image(img: np.ndarray, img_size: int, model: Darknet, conf_thresh: float, nms_thresh: float):
+def detect_image(img: Image, img_size: int, model: Darknet, conf_thresh: float, nms_thresh: float):
     # scale and pad image
     ratio = min(img_size/img.size[0], img_size/img.size[1])
     imw = round(img.size[0] * ratio)
@@ -41,9 +41,14 @@ def detect_image(img: np.ndarray, img_size: int, model: Darknet, conf_thresh: fl
     return detections[0]
 
 
-def post_process_detection(detections, classes) -> Iterable[Dict[str, float]]:
+def post_process_detection(detections, classes, pilimg: Image, img_size: int) -> Iterable[Dict[str, float]]:
 
     list_of_persons = []
+    img = np.array(pilimg)
+    pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
+    pad_y = max(img.shape[1] - img.shape[0], 0) * (img_size / max(img.shape))
+    unpad_h = img_size - pad_y
+    unpad_w = img_size - pad_x
     for x1, y1, x3, y3, conf, cls_conf, cls_pred in detections:
         cls = classes[int(cls_pred)]
         if cls == 'person':
@@ -85,17 +90,16 @@ def detect_persons(conf: Config) -> bool:
         pilimg = Image.fromarray(frame)
         detections = detect_image(pilimg, conf.img_size, model, conf.conf_thresh, conf.nms_thresh)
         if detections is not None:
-            persons = post_process_detection(detections, classes)
-            print(persons)
+            persons = post_process_detection(detections, classes, pilimg, conf.img_size)
             for person in persons:
                 row = {
-                    "frame": item.name,
+                    "frame": item.name.rstrip(".npy"),
                     "x1": person["x1"],
                     "x3": person["x3"],
                     "y1": person["y1"],
                     "y3": person["y3"]
                 }
-            result_df = result_df.append(row, ignore_index=True)
+                result_df = result_df.append(row, ignore_index=True)
 
     result_df.to_feather(f"{output_file_path}.feather")
 
@@ -117,7 +121,7 @@ if __name__ == "__main__":
     model.eval()
     classes = load_classes(class_path)
 
-    cap = cv2.VideoCapture('../data/girls_like_you_small.mp4')
+    cap = cv2.VideoCapture('../oh_oh_jaane_jaana.mp4')
 
     cmap = plt.get_cmap('tab20b')
     colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
@@ -151,7 +155,8 @@ if __name__ == "__main__":
                     y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
                     x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
                     x3 = x1 + box_w
-                    y3 = y1 + box_w
+                    y3 = y1 + box_h
+                    print(x1, y1, x3, y3)
                     list_of_persons.append({
                         'x1': x1,
                         'y1': y1,
@@ -184,5 +189,3 @@ if __name__ == "__main__":
     cap.release()
     cv2.destroyAllWindows()
 
-    with open('../data/detected_persons/result.hjson', 'w') as f:
-        hjson.dump(detected_persons, f)

pre_processor/sampler.py
@@ -27,8 +27,14 @@ def process_lyrics(lyrics: pd.DataFrame) -> pd.DataFrame:
 
 
 def resize(img: np.ndarray, new_res: int) -> np.ndarray:
-	width = int(img.shape[1] * new_res / img.shape[0])
-	height = int(img.shape[0] / new_res)
+
+	if img.shape[1] >= img.shape[0]:
+		width = int(img.shape[1] * new_res / img.shape[0])
+		height = new_res
+	else:
+		height = int(img.shape[0] * new_res / img.shape[1])
+		width = new_res
+
 	dim = (width, height)
 	resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 	return resized